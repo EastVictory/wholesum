74b595c248d434a9a8858d427078c9df3b00d6a2
LonelyTomb <garubav@gmail.com>
2023-06-18 12:03:05 +0100

    update: update padding

--- components/auth/AuthLayout.vue
+++ components/auth/AuthLayout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="auth-layout max-w-4xl py-[2.8125rem] mx-auto">
+  <div class="auth-layout max-w-4xl py-[1.875rem] mx-auto">
     <div class="logo-wrapper flex justify-center mb-[5.9375rem]">
-      <nuxt-icon name="logo" class="w-[1.9375rem] h-[1.8125rem]" filled />
+      <nuxt-link to="/">
+        <nuxt-icon name="logo" class="w-[1.9375rem] h-[1.8125rem]" filled />
+      </nuxt-link>
     </div>
     <div class="flex gap-[2.25rem] items-center">
       <div class="w-full lg:w-7/12">
