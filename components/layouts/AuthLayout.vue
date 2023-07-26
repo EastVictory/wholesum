133a8e8c80d9cd71cb5fa01f852832a94b4785a8
LonelyTomb <garubav@gmail.com>
2023-07-26 06:18:04 +0100

    update: update auth layout

--- components/layouts/AuthLayout.vue
+++ components/layouts/AuthLayout.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="auth-layout max-w-4xl py-[1.875rem] mx-auto">
-    <div class="logo-wrapper flex justify-center mb-[5.9375rem]">
-      <nuxt-link to="/">
-        <nuxt-icon name="logo" class="w-[1.9375rem] h-[1.8125rem]" filled />
+  <div class="auth-layout mx-auto bg-gunmetal">
+    <div class="logo-wrapper flex justify-center py-[2.65rem]">
+      <nuxt-link to="/" class="text-white">
+        <nuxt-icon
+          name="logo"
+          class="w-[1.9375rem] h-[1.8125rem] text-white"
+          filled
+        />
       </nuxt-link>
     </div>
-    <div class="flex gap-[2.25rem] items-center">
-      <div class="w-full lg:w-7/12">
+    <div class="auth-layout__contents">
+      <div class="w-full">
         <slot />
-      </div>
-      <div class="hidden lg:block image-wrapper flex-1">
-        <nuxt-icon name="meditation" filled />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.auth-layout {
+  &__contents {
+    @apply max-w-3xl bg-white rounded-lg shadow-[0.5rem_-0.5rem_0_#E88DBA];
+    @apply px-5 pt-[3.44rem] pb-[5.94rem] mb-20 mx-4 lg:mx-auto lg:mb-4;
+    @apply lg:pl-[7rem] lg:pr-[9.44rem] pt-[3.44rem] pb-[5.94rem];
+  }
+}
+</style>
