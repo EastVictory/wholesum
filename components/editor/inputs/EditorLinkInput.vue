92ea2380b0d317f7400f023623f2ad83fe712ff7
LonelyTomb <garubav@gmail.com>
2023-11-28 22:36:37 +0100

    update: update date display

--- components/editor/inputs/EditorLinkInput.vue
+++ components/editor/inputs/EditorLinkInput.vue
@@ -16,7 +16,7 @@ const handleSubmit = () => {
   emits("submit", {
     type: "link",
     content: url.value,
-    createdAt: DateTime.now().toFormat(`d LLL '"'yy '.' ta`),
+    createdAt: DateTime.now().toFormat(`d LLL '‘'yy '.' t a`),
   });
   url.value = "";
 };
