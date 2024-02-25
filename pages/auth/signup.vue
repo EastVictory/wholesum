323f541ea2ca91c95064399419a3af679c912f4b
LonelyTomb <garubav@gmail.com>
2024-02-25 21:06:25 +0100

    update: refactor auth

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -27,11 +27,13 @@ const processSubmit = () => {
 </script>
 
 <template>
-  <AuthLayout class="signup-layout layout">
+  <AuthLayout
+    class="signup-layout layout"
+    title="Create an account to get started"
+  >
     <Head>
       <Title>Sign Up</Title>
     </Head>
-    <h1 class="page-title">Create an account to get started</h1>
     <form
       class="flex flex-col gap-y-[3.125rem] mb-[3.25rem]"
       @submit.prevent="processSubmit"
@@ -108,7 +110,9 @@ const processSubmit = () => {
         <span class="text-center">Continue with Google</span>
       </button>
     </form>
-    <p class="mb-6 text-dark text-xs font-medium w-4/5 text-center mx-auto">
+    <p
+      class="mb-6 text-dark-puce text-lg font-medium w-4/5 text-center mx-auto"
+    >
       By continuing with Email or Google, you agree to Shie’s
       <nuxt-link class="underline hover:text-cardinal">
         Terms of Service
@@ -118,7 +122,7 @@ const processSubmit = () => {
         Privacy Policy
       </nuxt-link>
     </p>
-    <p class="text-dark text-xs font-medium text-center">
+    <p class="text-dark text-lg font-medium text-center">
       Already signed up?
       <nuxt-link to="/auth/login" class="underline hover:text-cardinal">
         Proceed to login
