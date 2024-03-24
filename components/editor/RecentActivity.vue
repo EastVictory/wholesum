644049d8e67febbf9d9537808c79455607cca18e
LonelyTomb <garubav@gmail.com>
2024-03-24 21:29:50 +0100

    update: add new note component

--- components/editor/RecentActivity.vue
+++ components/editor/RecentActivity.vue
@@ -3,30 +3,67 @@ const activities = [
   {
     title: "GROCERY LIST",
     duration: 26,
+    range: "2:13 - 2:47PM",
+  },
+  {
+    title: "CLEANING SCHEDULE",
+    duration: 26,
+    range: "2:13 - 2:47PM",
+  },
+  {
+    title: "WELL-NESS CHECKIN",
+    duration: 26,
+    range: "2:13 - 2:47PM",
   },
 ];
 </script>
 
 <template>
   <div class="daily-calendar">
-    <div class="flex justify-between items-center">
+    <div class="flex justify-between items-center mb-[2.6875rem]">
       <p class="text-2xl font-mono text-licorice">RECENT ACTIVITY</p>
       <nuxt-link class="shie-button px-3 bg-white gap-1 py-2" to="#">
         <span class="text-xs font-title h-2 text-black">ALL TODO</span>
         <nuxt-icon name="chevron-right" class="text-dark-puce h-2 w-2" filled />
       </nuxt-link>
     </div>
-    <section v-if="false" class="flex flex-col gap-3">
+    <section class="flex flex-col gap-3">
+      <div class="flex bg-white justify-between py-7 rounded-lg px-4">
+        <p class="font-title text-licorice h-4 text-sm">
+          🪷 <span>Day 4</span> • <span>DAILY STREAK</span>
+        </p>
+        <p class="font-title text-licorice h-4 text-xs">2 M ago</p>
+      </div>
       <div
         v-for="activity in activities"
         :key="activity.title"
-        class="flex bg-white justify-between"
+        class="flex bg-white justify-between py-7 rounded-lg px-4 hover:bg-conditioner cursor-pointer group"
       >
-        <p>
+        <p class="font-title text-licorice h-4">
           {{ activity.title }}
         </p>
-        <p>
-          {{ activity.duration }}
+        <p
+          class="inline-flex gap-4 text-licorice text-xs font-title h-4 items-center"
+        >
+          <span class="h-4"> {{ activity.duration }} M </span>
+          <span class="h-4">
+            {{ activity.range }}
+          </span>
+          <nuxt-icon
+            name="eye"
+            filled
+            class="w-4 hidden group-hover:inline-block h-4 mb-1"
+          />
+          <nuxt-icon
+            name="padlock"
+            filled
+            class="w-4 hidden group-hover:inline-block h-4 mb-2"
+          />
+          <nuxt-icon
+            name="dots-horizontal"
+            filled
+            class="w-4 hidden group-hover:inline-block h-4 mb-2"
+          />
         </p>
       </div>
     </section>
--- pages/editor/index.vue
+++ pages/editor/index.vue
@@ -4,26 +4,29 @@ import WeatherReport from "~/components/editor/WeatherReport.vue";
 import DailyCalendar from "~/components/editor/DailyCalendar.vue";
 import DailyCharts from "~/components/editor/DailyCharts.vue";
 import RecentActivity from "~/components/editor/RecentActivity.vue";
+import NewNote from "~/components/editor/NewNote.vue";
 </script>
 
 <template>
   <DashboardLayout title="Editor">
     <section class="flex">
       <div
-        class="w-full lg:w-1/2 pl-[9.4375rem] pr-[3.375rem] pt-4 editor-half"
+        class="w-full lg:!w-1/2 pl-[9.4375rem] pr-[3.375rem] pt-4 editor-half"
       >
         <WeatherReport class="mb-12" />
         <DailyCalendar class="mb-12" />
         <DailyCharts class="mb-12" />
         <RecentActivity />
       </div>
-      <div class="w-full lg:w-1/2"></div>
+      <div class="w-full lg:!w-1/2">
+        <NewNote />
+      </div>
     </section>
   </DashboardLayout>
 </template>
 
 <style scoped lang="scss">
 .editor-half {
-  @apply bg-[url('/images/svg/border-dashes.svg')] bg-no-repeat bg-right-top bg-contain;
+  @apply bg-[url('/images/svg/border-dashes.svg')] bg-repeat-y bg-right-top bg-auto;
 }
 </style>
